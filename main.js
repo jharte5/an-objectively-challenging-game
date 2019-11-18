ab2078f291c14a56527dbcbc5cdab187e1c1b6d5
Sung Paik <sung.paik@codeimmersives.com>
2019-11-18 16:02:09 -0500

    first function

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 function getAttackDamage(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-let currentMonster = monster
-document.getElementById("fight").addEventListener("click", getAttackDamage);
-monster.hitPoints - getAttackDamage
 
-function attackRound (getAttackDamage) {
-    players.hitPoints = players.hitPoints - getAttackDamage(min, max)
+let currentMonster = monster;
 
+document.getElementById("fight").addEventListener("click", attackRound);
+
+function attackRound () {
+    players.hitPoints = players.hitPoints - getAttackDamage(monster.attackMin, monster.attackMax)
+    currentMonster.hitPoints = currentMonster.hitPoints - getAttackDamage(player.attackMin, player.attackMax)
 }
