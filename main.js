c35fd39ef2cf4fe0690cd6d21d1fcaa2c7948ae7
jharte5 <jonathan.harte@codeimmersives.com>
2019-11-18 15:54:51 -0500

    creating functions

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 function getAttackDamage(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+let currentMonster = monster
+document.getElementById("fight").addEventListener("click", getAttackDamage);
+monster.hitPoints - getAttackDamage
+
+function attackRound (getAttackDamage) {
+    
+}
